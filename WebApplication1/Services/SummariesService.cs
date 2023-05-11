78731575a16756d861e9c6ad45ee9aceab4a0e57
NavaMaklev <131261211+NavaMaklev@users.noreply.github.com>
2023-05-11 14:17:09 +0300

    Update SummariesService.cs
    I added an explanation of the query.

--- WebApplication1/Services/SummariesService.cs
+++ WebApplication1/Services/SummariesService.cs
@@ -5,7 +5,6 @@ using System.Data.SqlClient;
 using System.Data;
 using Newtonsoft.Json.Linq;
 
-
 namespace CoronaSystemApp.Services
 {
     public class SummariesService
@@ -33,6 +32,32 @@ namespace CoronaSystemApp.Services
         public List<DateAndNumberOfActiveSicks> GetDateAndNumberOfActiveSicks()
         {
             SqlConnection con = new SqlConnection(_configuration.GetConnectionString("dbcon").ToString());
+            /* 
+            Explanation of the query:
+            1. Common Table Expression (CTE) Definition:
+            - The query begins with a CTE definition named "Dates". CTEs are temporary result sets that can be referenced within the main query.
+            - The "Dates" CTE generates a sequence of dates starting from the first day of the previous month and ending on the last day of the current month.
+            - It uses recursive logic to generate a series of dates incrementally.
+            2. Recursive Part of the CTE:
+            - The initial part of the CTE specifies the starting date as the last day of the previous month using the `DATEADD` and `DATEDIFF` functions.
+            - The recursive part selects the next date by adding one day to the previous date.
+            - The recursive part is controlled by the condition in the WHERE clause, which ensures that the loop continues until the current date reaches the last day of the current month.
+            3. Main Query:
+            - The main query selects data from the "Dates" CTE and performs additional operations.
+            - It selects the "CurrentDate" column from the "Dates" CTE and aliases it as "Date".
+            - It counts the number of rows in the "disease" table where the "CurrentDate" falls within the range of the "StartDate" and "EndDate" columns.
+            - The `LEFT JOIN` ensures that all dates from the "Dates" CTE are included in the result, even if there are no matching rows in the "disease" table.
+            - The result is grouped by "CurrentDate" and the count of diseases is calculated for each date.
+            - The result is ordered by "CurrentDate" in ascending order.
+            4. `OPTION (MAXRECURSION 0)`:
+            - This option is used to specify the maximum recursion level for the recursive CTE.
+            - Setting it to 0 means there is no limit on the number of recursive iterations.
+            In summary, this query generates a sequence of dates within a given range,
+            starting from the first day of the previous month and ending on the last day of the current month. 
+            It then counts the number of active patients for each date by checking if the date falls within the 
+            range defined by the "StartDate" and "EndDate" columns in the "disease" table. The result is a list of
+            dates and the corresponding count of active patients for each date, ordered by date.
+            */
             string query = @"
                 WITH Dates AS (
                 SELECT DATEADD(DAY, -1, DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()) - 1, 0)) AS CurrentDate
